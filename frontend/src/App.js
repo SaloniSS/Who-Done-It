4d3e3c53bbb82b23b79b1e62355961079e059954
SaloniSS <sxs170015@utdallas.edu>
2020-12-12 21:25:54 -0600

    ;-;

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,38 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import "./App.css";
 import "antd/dist/antd.css";
 import { Layout } from "antd";
 import { Profiles } from "./components/Profiles";
+import { MatchDetective } from "./pages/MatchDetective";
 const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <Layout>
-        <Sider style={{ padding: 10 }}>
-          <Profiles />
-        </Sider>
+    <Router>
+      <div className="App">
         <Layout>
-          <Header>Header aka name/lvl</Header>
-          <Content>Content</Content>
-          <Footer>enter answer here?</Footer>
+          <Sider style={{ padding: 10 }}>
+            <Profiles />
+          </Sider>
+          <Layout>
+            <Header>Header aka name/lvl</Header>
+            <Content>Content</Content>
+            <Footer>enter answer here?</Footer>
+          </Layout>
         </Layout>
-      </Layout>
-    </div>
+        <Switch>
+          <Route path="/lvl-1" exact>
+            <MatchDetective />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
