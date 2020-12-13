89a9aba8d4386a8d5799512c431c9ee528e545c8
medhajon <msj170000@utdallas.edu>
2020-12-13 02:42:09 -0600

    Level 4
    Saloni look plz.....

--- frontend/src/components/LevelFour.js
+++ frontend/src/components/LevelFour.js
@@ -1,7 +1,50 @@
-const LevelFour = () => {
+import { Comment, List, Select, Button } from "antd";
+import { useState } from "react";
+import endcard from "../endcard.png";
+const { Option } = Select;
+
+
+
+const LevelFour = (props) => {
+  const [answer, setAnswer] = useState(null);
+  const [selected, setSelected] = useState("Butler");
+
+  const handleChange = (value) => {
+    setSelected(value);
+  };
+
+  const questionSomeone = () => {
+    if(selected == "Gadget")
+    {
+      setAnswer("You accused "+ selected +". He is the murderer! Before he could escape, the rest of the detectives cornered him, and you called police! You saved the day. As for Holmes, he is taken to the hospital and is recovering. Happy ending!");
+    }
+    else
+      setAnswer("You accused " + selected + ". Unfortunately, you chose the wrong person. Holmes managed to live for a few minutes to tell you all that he knew who would kill him. As soon as you realized who the murderer was, he broke into your house and killed you and the rest of the detectives. Fin." );
+  }; 
   return (
     <>
-      <h2>lvl4</h2>
+      <h2>Who Done it?</h2>
+      <p>
+        After listening to everyone's alibis, you took all the missing pieces together. Who killed Sherlock Holmes? 
+      </p>
+      
+      <Select
+        defaultValue="Butler"
+        style={{ width: 120 }}
+        onChange={handleChange}
+      >
+        <Option value="Watson">Watson</Option>
+        <Option value="Nancy">Nancy</Option>
+        <Option value="Clouseau">Clouseau</Option>
+        <Option value="Velma">Velma</Option>
+        <Option value="Gadget">Gadget</Option>
+        <Option value="Butler">Butler</Option>
+      </Select>
+      <Button onClick={questionSomeone}>Accuse</Button>
+       <br />
+      {answer && <h3>{answer}</h3>} 
+      <br />  <br />
+      <img src={endcard} width="70%" />
     </>
   );
 };
