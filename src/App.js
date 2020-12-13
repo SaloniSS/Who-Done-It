776d3f13a80f5a0bfa7ba4d48325f5d91bbe2387
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-12-12 18:39:55 -0600

    add accord

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import "antd/dist/antd.css";
 import { Layout } from "antd";
+import { Profiles } from "./components/Profiles";
 const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <Sider>Sider aka profiles</Sider>
+        <Sider>
+          <Profiles />
+        </Sider>
         <Layout>
           <Header>Header aka name/lvl</Header>
           <Content>Content</Content>
---
+++ src/components/Profiles.js
@@ -0,0 +1,24 @@
+import { Collapse } from "antd";
+const { Panel } = Collapse;
+
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+
+export const Profiles = () => {
+  return (
+    <Collapse accordion>
+      <Panel header="Sherlock Holmes" key="1">
+        <p>{text}</p>
+      </Panel>
+      <Panel header="Watson" key="2">
+        <p>{text}</p>
+      </Panel>
+      <Panel header="Nancy Drew" key="3">
+        <p>{text}</p>
+      </Panel>
+    </Collapse>
+  );
+};
