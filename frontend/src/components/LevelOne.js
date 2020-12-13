5ed0456ccb8f41049d44c7dc52c542c43e814d19
SaloniSS <sxs170015@utdallas.edu>
2020-12-12 21:43:30 -0600

    hello

--- frontend/src/components/LevelOne.js
+++ frontend/src/components/LevelOne.js
@@ -1,3 +1,14 @@
+import { useState, useEffect } from "react";
+
 export const LevelOne = () => {
-  return <h2>lvl1</h2>;
+  let [detective, setDetective] = useState("");
+  let [image, setImage] = useState("");
+
+  return (
+    <div>
+      <h1>Upload an Image Dressed as a Detective</h1>
+      <button>Upload</button>
+      {detective && <p>You most look like {detective}</p>}
+    </div>
+  );
 };
