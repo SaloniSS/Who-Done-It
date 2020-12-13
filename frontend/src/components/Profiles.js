923da7bc7e1775084886c19ebe75ece942b3ec09
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-12-12 21:24:25 -0600

    make profs

--- frontend/src/components/Profiles.js
+++ frontend/src/components/Profiles.js
@@ -7,34 +7,69 @@ const text = `
   it can be found as a welcome guest in many households across the world.
 `;
 
+const detectives = [
+  {
+    name: "Sherlock Holmes",
+    image:
+      "https://imgix.bustle.com/inverse/8d/2e/cc/37/6ca2/41c7/81cc/8c9a28f2dfbc/cumberbatch-holmesjpg.jpeg?w=1200&h=630&q=70&fit=crop&crop=faces&fm=jpg",
+    description: "hi",
+  },
+  {
+    name: "John Watson",
+    image:
+      "https://imgix.bustle.com/inverse/8d/2e/cc/37/6ca2/41c7/81cc/8c9a28f2dfbc/cumberbatch-holmesjpg.jpeg?w=1200&h=630&q=70&fit=crop&crop=faces&fm=jpg",
+    description: "hi",
+  },
+  {
+    name: "Nancy Drew",
+    image:
+      "https://imgix.bustle.com/inverse/8d/2e/cc/37/6ca2/41c7/81cc/8c9a28f2dfbc/cumberbatch-holmesjpg.jpeg?w=1200&h=630&q=70&fit=crop&crop=faces&fm=jpg",
+    description: "hi",
+  },
+  {
+    name: "Detective Clouseau",
+    image:
+      "https://imgix.bustle.com/inverse/8d/2e/cc/37/6ca2/41c7/81cc/8c9a28f2dfbc/cumberbatch-holmesjpg.jpeg?w=1200&h=630&q=70&fit=crop&crop=faces&fm=jpg",
+    description: "hi",
+  },
+  {
+    name: "Velma Dinkley",
+    image:
+      "https://imgix.bustle.com/inverse/8d/2e/cc/37/6ca2/41c7/81cc/8c9a28f2dfbc/cumberbatch-holmesjpg.jpeg?w=1200&h=630&q=70&fit=crop&crop=faces&fm=jpg",
+    description: "hi",
+  },
+  {
+    name: "Inspector Gadget",
+    image:
+      "https://imgix.bustle.com/inverse/8d/2e/cc/37/6ca2/41c7/81cc/8c9a28f2dfbc/cumberbatch-holmesjpg.jpeg?w=1200&h=630&q=70&fit=crop&crop=faces&fm=jpg",
+    description: "hi",
+  },
+];
+
 export const Profiles = () => {
   return (
     <>
       <h2>Profiles</h2>
       <Collapse accordion>
-        <Panel header="Sherlock Holmes" key="1">
-          <p>{text}</p>
-        </Panel>
-        <Panel
-          header={
-            <div
-              style={{
-                display: "flex",
-                direction: "flex-row",
-                justifyContent: "space-between",
-              }}
-            >
-              <p>Watson</p>
-              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-            </div>
-          }
-          key="2"
-        >
-          <p>{text}</p>
-        </Panel>
-        <Panel header="Nancy Drew" key="3">
-          <p>{text}</p>
-        </Panel>
+        {detectives.map((dect, i) => (
+          <Panel
+            header={
+              <div
+                style={{
+                  display: "flex",
+                  direction: "flex-row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <p>{dect.name}</p>
+                <Avatar src={dect.image} />
+              </div>
+            }
+            key={i}
+          >
+            <p>{dect.description}</p>
+          </Panel>
+        ))}
       </Collapse>
     </>
   );
