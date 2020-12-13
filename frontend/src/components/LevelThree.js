cbe56770ffd2f42d12a7b4c8364ba79e8847d7c6
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-12-12 23:37:29 -0600

    add questioning

--- frontend/src/components/LevelThree.js
+++ frontend/src/components/LevelThree.js
@@ -1,4 +1,6 @@
-import { Comment, List } from "antd";
+import { Comment, List, Select, Button } from "antd";
+import { useState } from "react";
+const { Option } = Select;
 
 const data = [
   {
@@ -26,20 +28,43 @@ const data = [
 ];
 
 const LevelThree = () => {
+  const [answer, setAnswer] = useState(null);
+
+  function handleChange(value) {
+    console.log(`selected ${value}`);
+  }
+
   return (
-    <List
-      itemLayout="horizontal"
-      dataSource={data}
-      renderItem={(item) => (
-        <li>
-          <Comment
-            author={item.author}
-            avatar={item.avatar}
-            content={item.content}
-          />
-        </li>
-      )}
-    />
+    <>
+      <List
+        itemLayout="horizontal"
+        dataSource={data}
+        renderItem={(item) => (
+          <li>
+            <Comment
+              author={item.author}
+              avatar={item.avatar}
+              content={item.content}
+            />
+          </li>
+        )}
+      />
+      <h3>You can pick two people to ask follow-up questions:</h3>
+      <Select
+        defaultValue="lucy"
+        style={{ width: 120 }}
+        onChange={handleChange}
+      >
+        <Option value="Watson">Watson</Option>
+        <Option value="Nancy">Nancy</Option>
+        <Option value="Clouseau">Clouseau</Option>
+        <Option value="Velma">Velma</Option>
+        <Option value="Gadget">Gadget</Option>
+        <Option value="Butler">Butler</Option>
+      </Select>
+      <Button>Question</Button>
+      {answer && answer}
+    </>
   );
 };
 
